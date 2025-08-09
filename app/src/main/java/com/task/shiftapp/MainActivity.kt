eb896ca658293479c5d91ac60fbcd591b2fb1ca5
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2025-08-10 01:02:48 +0300

    adding and removing users

--- app/src/main/java/com/task/shiftapp/MainActivity.kt
+++ app/src/main/java/com/task/shiftapp/MainActivity.kt
@@ -1,20 +1,126 @@
 package com.task.shiftapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.task.shiftapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import com.task.shiftapp.data.api.ApiClient.Companion.api
+import com.task.shiftapp.data.model.user.User
+import com.task.shiftapp.ui.UserAdapter
+import kotlinx.coroutines.withContext
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = UserAdapter()
+    private val users = mutableListOf<User>()
+    private var isAddPressed = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupRecyclerView()
+        setupClickListeners()
+    }
+
+    private fun setupRecyclerView() = with(binding.rcUsers) {
+        layoutManager = LinearLayoutManager(this@MainActivity)
+        adapter = this@MainActivity.adapter
+
+        addOnItemTouchListener(object : RecyclerView.SimpleOnItemTouchListener() {
+            override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                hideInputAndClear()
+                return false
+            }
+        })
+    }
+
+    private fun setupClickListeners() = with(binding) {
+        btnAddUsers.setOnClickListener {
+            if (isAddPressed) {
+                val count = edUserCount.text.toString().toIntOrNull() ?: 1
+                if (count in 1..20) {
+                    addUsersToRC(count)
+                    hideInputAndClear()
+                } else {
+                    edUserCount.text.clear()
+                }
+            } else {
+                showInput()
+            }
         }
+
+        btnRemoveUsers.setOnClickListener {
+            removeAllUsers()
+        }
+
+    }
+
+    private fun addUsersToRC(num: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val newUsers = (1..num).mapNotNull {
+                    api.getRandomUser().results.firstOrNull()
+                }
+
+                withContext(Dispatchers.Main) {
+                    val updatedList = ArrayList(users).apply { addAll(newUsers) }
+                    users.clear()
+                    users.addAll(updatedList)
+                    adapter.submitList(updatedList)
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun removeAllUsers() {
+        if (users.isNotEmpty()) {
+            users.clear()
+            adapter.submitList(emptyList())
+        } else {
+            Toast.makeText(this, "Список пуст.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun showInput() = with(binding) {
+        edUserCount.visibility = View.VISIBLE
+        isAddPressed = true
+
+        edUserCount.requestFocus()
+        showKeyboard()
+    }
+
+    private fun hideInputAndClear() = with(binding) {
+        if (isAddPressed) {
+            edUserCount.visibility = View.GONE
+            edUserCount.text.clear()
+            hideKeyboard()
+            isAddPressed = false
+        }
+    }
+
+    private fun showKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.edUserCount, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.edUserCount.windowToken, 0)
     }
 }
