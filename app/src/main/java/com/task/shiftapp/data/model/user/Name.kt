5da6bac6f14981460fd254441ff61d891d4771ff
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2025-08-09 17:55:27 +0300

    create model-user

---
+++ app/src/main/java/com/task/shiftapp/data/model/user/Name.kt
@@ -0,0 +1,7 @@
+package com.task.shiftapp.data.model.user
+
+data class Name(
+    val title: String,
+    val first: String,
+    val last: String,
+)
---
+++ app/src/main/java/com/task/shiftapp/data/model/user/User.kt
@@ -0,0 +1,8 @@
+package com.task.shiftapp.data.model.user
+
+data class User (
+    val name: Name,
+    val location: Location,
+    val phone: String,
+    val picture: Picture,
+)
