a2703f9c71a0483fce1468d822625b55b820e3b2
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2025-08-10 01:49:19 +0300

    saving and loading users <- SharedPreferences

--- app/src/main/java/com/task/shiftapp/utils/Constants.kt
+++ app/src/main/java/com/task/shiftapp/utils/Constants.kt
@@ -2,4 +2,6 @@ package com.task.shiftapp.utils
 
 object Constants {
     const val BASE_URL = "https://randomuser.me/"
+    const val USER_PREF = "UserPref"
+    const val USER_LIST_KEY = "UserListKey"
 }
