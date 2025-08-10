d901b49e66319de7a3c3a560c7264dfa8b488f45
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2025-08-10 19:33:15 +0300

    create profile activity

---
+++ app/src/main/java/com/task/shiftapp/ProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.task.shiftapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.task.shiftapp.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
